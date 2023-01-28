9f6deb77fb1cb684e7152d4a573fb3286ce9dc30
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-01-28 21:50:46 +0100

    add: path to saracen icon

--- take_screenshot.py
+++ take_screenshot.py
@@ -1,6 +1,19 @@
 import pyautogui
+import os
 
 def takeScreenshot():
-    entire_map = pyautogui.screenshot()
-    entire_map.save('map.png')
+    screenshot_folder = r'C:\Users'
+    screenshot_name = 'screenshot.png'
+    screenshot_path = os.path.join(screenshot_folder, screenshot_name)
     
+    pyautogui.alert('This is an alert box.')
+    desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    
+    folder_path = os.path.join(desktop_path, "New folder")
+    
+    image_path = os.path.join(folder_path, "image.png")
+    saracenIconLocation = pyautogui.locateOnScreen('image_path')
+    return saracenIconLocation
+
+
+print(takeScreenshot())
