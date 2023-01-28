a43ea6034cf8af0e2b1627d183542fab20ddd7b7
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-01-28 22:29:42 +0100

    add: cv2 to compare images

--- take_screenshot.py
+++ take_screenshot.py
@@ -1,19 +1,29 @@
 import pyautogui
 import os
+import cv2
 
 def takeScreenshot():
-    screenshot_folder = r'C:\Users'
+    screenshot_folder = r'C:\Users\idk\backseat-gAOEmer\Screenshots'
     screenshot_name = 'screenshot.png'
     screenshot_path = os.path.join(screenshot_folder, screenshot_name)
+    screenshot = pyautogui.screenshot()
+
+    # Save the screenshot to a file
+    screenshot.save(screenshot_path)
+
+    # image we are looking for 
+    folder_path = r'C:\Users\Desktop\image_folder'
+    #image_path = os.path.join(folder_path, "image.png")
     
-    pyautogui.alert('This is an alert box.')
-    desktop_path = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+    image1 = cv2.imread(os.path.join(folder_path, 'image.png'))
+    image2 = cv2.imread(os.path.join(screenshot_path, 'screenshot.png'))
     
-    folder_path = os.path.join(desktop_path, "New folder")
+    result = cv2.matchTemplate(image1, image2, cv2.TM_CCOEFF_NORMED)
     
-    image_path = os.path.join(folder_path, "image.png")
-    saracenIconLocation = pyautogui.locateOnScreen('image_path')
-    return saracenIconLocation
+    if cv2.minMaxLoc(result)[1] > 0.9:
+        print("Images match")
+    else:
+        print("Images do not match")
 
 
-print(takeScreenshot())
+takeScreenshot()
