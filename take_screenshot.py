c0c0bc66840d13cdbdb698b62e7612d0a95509e9
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-02-04 20:24:40 +0100

    changes: takes a screenshot of a specified location

--- take_screenshot.py
+++ take_screenshot.py
@@ -3,27 +3,11 @@ import os
 import cv2
 
 def takeScreenshot():
-    screenshot_folder = r'C:\Users\idk\backseat-gAOEmer\Screenshots'
-    screenshot_name = 'screenshot.png'
-    screenshot_path = os.path.join(screenshot_folder, screenshot_name)
-    screenshot = pyautogui.screenshot()
-
-    # Save the screenshot to a file
-    screenshot.save(screenshot_path)
-
-    # image we are looking for 
-    folder_path = r'C:\Users\Desktop\image_folder'
-    #image_path = os.path.join(folder_path, "image.png")
+    # makes sure 'economy mode' is active
+    pyautogui.hotkey("ctrl", "r")
     
-    image1 = cv2.imread(os.path.join(folder_path, 'image.png'))
-    image2 = cv2.imread(os.path.join(screenshot_path, 'screenshot.png'))
+    # rough location of sell and buy prices
+    MktAbuse = pyautogui.screenshot(region=(2048, 1080, 500, 200))
+    MktAbuse.save(r'C:\Users\idk\backseat-gAOEmer\Screenshots\MktAbuse.jpg')
     
-    result = cv2.matchTemplate(image1, image2, cv2.TM_CCOEFF_NORMED)
-    
-    if cv2.minMaxLoc(result)[1] > 0.9:
-        print("Images match")
-    else:
-        print("Images do not match")
-
-
 takeScreenshot()
